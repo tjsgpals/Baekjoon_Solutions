6b18776bca4b7fa535f5ec24dc8d8dcce739b3b2
tjsgpals <sunhm1023@naver.com>
2025-08-02 23:16:01 +0900

    feat: Add solution for problem 10952

---
+++ 10952/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;     //숫자 시행 시 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        while (a!=0 || b!=0) {
+            System.out.println(a+b);
+            a = sc.nextInt();
+            b = sc.nextInt();
+        }
+        sc.close();
+    }
+}
